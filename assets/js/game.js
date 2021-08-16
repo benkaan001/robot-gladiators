233a34138fc784c67da14ee70761ea812b32264e
Ben Kaan <benkaan001@gmail.com>
2021-08-15 22:32:07 -0500

    add window alert

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alret! Javascript is running!");
+window.prompt("What is your robot's name?")
+
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!")
+}
+fight();
